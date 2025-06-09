390fb028714dcafa68c0345c0ccd2206833a5f02
adriannakla <s30544@pjwstk.edu.pl>
2025-06-09 13:24:20 +0200

    Zajecia 09.06

--- 2425L_POXPG1_11c_Projekt2-main/2425L_POXPG1_11c_Projekt2-main/Assets/Scripts/PlayerStats.cs
+++ 2425L_POXPG1_11c_Projekt2-main/2425L_POXPG1_11c_Projekt2-main/Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -41,7 +42,15 @@ public class PlayerStats : MonoBehaviour
         {
             anim.SetTrigger("isDead");
             isDead = true;
+            Invoke(nameof(ReloadScene), 2);
+
         }
-    }
+
+       
+    } 
+    private void ReloadScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
 }
